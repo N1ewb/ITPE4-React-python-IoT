0c96c6c5a633d35d0faf384b4347e9ee2f671a01
Nathaniel A Lucero <92198311+N1ewb@users.noreply.github.com>
2024-10-14 17:33:16 +0800

    fixed error in lgin page

--- client/src/auth/Login.tsx
+++ client/src/auth/Login.tsx
@@ -38,8 +38,8 @@ const Login: React.FC = () => {
       <h1 className="text-indigo-800 text-4xl font-bold text-center mb-6">Login</h1>
       <input 
         type="text" 
-        placeholder="Username" 
-        ref={usernameRef} 
+        placeholder="email" 
+        ref={emailRef} 
         className="border border-indigo-600 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-indigo-400"
       />
       <input 
